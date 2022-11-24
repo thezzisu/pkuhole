63e9a40eba97011b8650fc65fe4493e3051c8ad8
thezzisu <thezzisu@gmail.com>
2022-11-24 21:02:46 +0800

    feat: support comments

---
+++ src/components/CommentCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="border shadow m-4 p-4">
+    <div class="flex items-center justify-between pb-4">
+      <code>{{ comment.cid }}</code>
+      <code>
+        Anon {{ props.comment.anonymous }} Name {{ props.comment.name }}
+      </code>
+    </div>
+    <hr />
+    <article class="p-2" v-html="props.comment.text"></article>
+    <hr />
+    <div>
+      <code>
+        {{ (Date as any)(+props.comment.timestamp).toLocaleString() }}
+        {{ props.comment.tag }}
+        {{ props.comment.islz ? 'lz' : '' }}
+      </code>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { IComment } from '@/api'
+
+const props = defineProps<{
+  comment: IComment
+}>()
+</script>
--- src/views/HoleView.vue
+++ src/views/HoleView.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="w-full flex flex-col items-stretch">
-    <HoleCard v-if="res.state.value" :hole="res.state.value.hole" />
+    <template v-if="res.state.value">
+      <HoleCard :hole="res.state.value.hole" />
+      <CommentCard
+        v-for="comment in res.state.value.comments"
+        :key="comment.cid"
+        :comment="comment"
+      />
+    </template>
     <div v-else class="p-4 text-xl font-bold">Loading...</div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { getHole } from '@/api'
+import { getCommentList, getHole } from '@/api'
+import CommentCard from '@/components/CommentCard.vue'
 import HoleCard from '@/components/HoleCard.vue'
 import { useAsyncState } from '@/compose/async'
 
@@ -16,8 +24,10 @@ const props = defineProps<{
 
 const res = useAsyncState(async () => {
   const resp = await getHole(props.pid)
+  const comments = await getCommentList(props.pid)
   return {
-    hole: resp.data
+    hole: resp.data,
+    comments: comments.data
   }
 }, null)
 
